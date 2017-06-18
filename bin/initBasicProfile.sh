311e29b1c4e6d8d0681862aacee395a79cfa11d5
mcotera <cotera.miguel.03@gmail.com>
2017-06-17 20:05:09 -0500

    add bin directory with two shell script

---
+++ bin/initBasicProfile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd 
+mkdir -p ~/Devel/Project/Customer/mcotera
+mkdir -p ~/Devel/practice
+mkdir ~/others
+mkdir ~/bin
+mkdir ~/tmp
+
---
+++ bin/newProject.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo $1
+
+mkdir $1
+cd $1
+mkdir eWS
+mkdir Firmware
+mkdir Hardware
+mkdir Software
+mkdir doc
+
