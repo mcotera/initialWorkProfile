702e80778ffc24c756592e9332491e032b427e3a
mcotera <cotera.miguel.03@gmail.com>
2018-04-18 18:12:34 -0500

    RPi tools link added

--- bin/initBasicProfile.sh
+++ bin/initBasicProfile.sh
@@ -3,10 +3,10 @@
 cd 
 mkdir -p ~/Devel/Projects/Customer/mcotera
 mkdir -p ~/Devel/RPi/sysroot
-mkdir -p ~/Devel/RPi/tools
 mkdir -p ~/Devel/practice
 mkdir ~/others
 mkdir ~/bin
 mkdir ~/tmp
 chmod -R ugo+rwx ~/Devel/RPi/sysroot
-
+mkdir /opt/RPi
+ln -s /opt/RPi/tools /home/mcotera/Devel/RPi/.
