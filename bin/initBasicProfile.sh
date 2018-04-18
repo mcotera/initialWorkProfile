9343f91af684fe4db237432ced29986cc514627a
mcotera <cotera.miguel.03@gmail.com>
2018-04-18 15:50:06 -0500

    sysroot and tools added
    sysroot added, it is important to sshfs

--- bin/initBasicProfile.sh
+++ bin/initBasicProfile.sh
@@ -2,8 +2,11 @@
 
 cd 
 mkdir -p ~/Devel/Projects/Customer/mcotera
+mkdir -p ~/Devel/RPi/sysroot
+mkdir -p ~/Devel/RPi/tools
 mkdir -p ~/Devel/practice
 mkdir ~/others
 mkdir ~/bin
 mkdir ~/tmp
+chmod -R ugo+rwx ~/Devel/RPi/sysroot
 
